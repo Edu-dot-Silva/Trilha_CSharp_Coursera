d2c64247f3465a7f64008fd4ad1d604b4e21e810
Edu-dot-Silva <eduardo_esilva@hotmail.com>
2026-05-14 19:40:40 -0300

    inciando fundamentos de componentes blazor

---
+++ C#Fundamentals/IntroductiontoBlazor/BlazorComponentDemo/Models/Product.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorComponentDemo.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }   
+        public string Description { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+    }
+}
